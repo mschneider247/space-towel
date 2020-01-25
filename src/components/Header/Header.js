4770c67580e9ed2b0bf10d259376368b1ccaf9cc
Michael <mschneider247@gmail.com>
2020-01-24 19:36:51 -0700

    Add extra components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>SPACE TOWEL</h1>
+      <img alt='Space Towel LOGO'/>
+    </div>
+  )
+};
+
+export default Header;

4a8d40c5a40afb8df45e3490356a0ae970e5347d
Michael <mschneider247@gmail.com>
2020-02-06 16:28:31 -0700

    Add styling to Header and Bio

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Header.css'
+import './Bio.css'
 import gitHub from '../../images/github.svg'
 import linkedIn from '../../images/linkedin.png'
 import bioPic from '../../images/MSchneiderPic.jpg'
@@ -9,20 +9,9 @@ const Bio = () => {
   return (
     <section className="bio_section">
       <div className="bio_div">
-        <h1 id="name">Michael Schneider</h1>
-        <h1 id="title">Front End Developer</h1>
         <div className="bio_div-content">
           <div className="bio_div-pic">
             <img className="bio_pic" src={bioPic} alt='Bio Pic'/>
-          </div>
-          <div className="bio_div-social">
-            <p>mschneider247@gmail.com</p>
-            <a href="https://github.com/mschneider247">
-              <img className="social_icon" src={gitHub} alt="GitHub"/>
-            </a>
-            <a href="https://www.linkedin.com/in/mschneider247/">
-              <img className="social_icon" src={linkedIn} alt="LinkedIn"/>
-            </a>
           </div>
         </div>
         <h3>About Me:</h3>

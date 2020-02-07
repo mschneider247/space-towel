f81a38b852f3393c4e66a8408df2b4b32a49901b
Michael <mschneider247@gmail.com>
2020-02-06 17:44:12 -0700

    Change hover state for buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -34,9 +34,6 @@ class App extends Component {
           <section>
             <Bio />
           </section>
-          <section>
-            <Content />
-          </section>
         </main>
       </section>
     );

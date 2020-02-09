a0e3af8ae4bd8b7b8c6b6ebb7aea0d0bcc3e1fc8
Michael <mschneider247@gmail.com>
2020-02-09 16:37:45 -0700

    Add images to SpaceFarmer

--- src/components/SpaceFarmer/SpaceFarmer.js
+++ src/components/SpaceFarmer/SpaceFarmer.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import './SpaceFarmer.css'
+import './SpaceFarmer.css';
+import sf1 from '../../images/SF1.jpg';
+import sf2 from '../../images/SF2.jpg';
+import sf3 from '../../images/SF3.jpg';
+import sf4 from '../../images/SF4.jpg';
+import sf5 from '../../images/SF5.jpg';
 
 const SpaceFarmer = () => {
 
@@ -7,17 +12,29 @@ const SpaceFarmer = () => {
     <section className="spaceFarmer_section">
       <section className="spaceFarmer_images">
         <h1>Space Farmer!!</h1>
-        <img alt="img1"/>
-        <img alt="img2"/>
-        <img alt="img3"/>
-        <img alt="img4"/>
-        <img alt="img5"/>
+        <img className="tRow" src={sf1} alt="img1"/>
+        <img className="tRow" src={sf2} alt="img2"/>
+        <img className="bRow" src={sf3} alt="img3"/>
+        <img className="bRow" src={sf4} alt="img4"/>
+        <img className="bRow" src={sf5} alt="img5"/>
       </section>
       <section className="spaceFarmer_info">
         <div>
           <ul>
+            <h3>
+              Tech Used:
+            </h3>
             <li>
-              huh?
+              tech 1
+            </li>
+            <li>
+              tech 2
+            </li>
+            <li>
+              tech 3
+            </li>
+            <li>
+              tech 4
             </li>
           </ul>
         </div>

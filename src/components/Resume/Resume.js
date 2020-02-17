69a08eb2d00bc5c4fcb15e21bac84bcaee775982
Michael <mschneider247@gmail.com>
2020-02-17 11:05:50 -0700

    Remove unused function

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 
 class Resume extends Component {
 
-  componentDidMount = () => {
-    this.props.updateTopic('turd')
-  }
-
   render () {
     return (
       <section>
